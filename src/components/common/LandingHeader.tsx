6665a233f0615f0442ae0eccb87469770e031a6d
Elizabeth Okorie <elizabethokorie407@gmail.com>
2023-10-26 16:51:15 +0100

    header nearly perfect

--- src/components/common/LandingHeader.tsx
+++ src/components/common/LandingHeader.tsx
@@ -2,17 +2,24 @@ import {AiFillFacebook, AiOutlineInstagram, AiOutlineMenu, AiOutlineTwitter} fro
 import { useDispatch, useSelector } from "react-redux";
 import Register from "../../pages/auth/Register";
 import SignIn from "../../pages/auth/SignIn";
-import { toggleState, toggleState2, toggleState3 } from "../../global/GlobalState";
+import { logOut, toggleState, toggleState2, toggleState3 } from "../../global/GlobalState";
 import LandingDropDown from "./LandingDropDown";
+import { useState } from "react";
 
 
 
 const LandingHeader = () => {
+  const [drop, setDrop] = useState<boolean>(false)
     const dispatch = useDispatch()
     const user = useSelector((state : any) => state.user)
     const toggle = useSelector((state: any) => state.toggle);
   const toggle2 = useSelector((state: any) => state.toggle2);
   const toggle3 = useSelector((state: any) => state.toggle3);
+
+  const onDrop = () =>{
+    setDrop(!drop)
+  }
+
 
   return (
     <>
@@ -46,10 +53,42 @@ const LandingHeader = () => {
                        </>
                           ) : (
 
-                            <div className=" w-[110px] h-[38px] bg-black flex justify-center items-center text-[17px] rounded-full text-white font-[700] hover:bg-slate-900">Hey, {user?.name} Henry</div>
+                            <div className=" w-[110px] h-[38px] bg-black flex justify-center items-center text-[17px] rounded-full text-white font-[700] hover:bg-slate-900"
+                            onClick={()=>{
+                              onDrop()
+                            }}
+                            >Hey, {user?.name} Henry</div>
                           )
                         }
                 </div>
+
+                {
+                  user? (
+                    <>
+                      {
+                  drop ? (
+                    <>
+                      <div className="w-[180px] rounded-md bg-white shadow-2xl absolute right-[90px] top-[65px] z-[100] text-[13px] font-[700] headerMedium:hidden flex flex-col ">
+                 <div className="w-full h-[40px] hover:text-white  hover:bg-blue-700 cursor-pointer flex items-center px-3 rounded-t-md">Profile</div>
+
+                 <div className="w-full h-[40px] hover:text-white  hover:bg-blue-700 cursor-pointer flex items-center px-3">Account Settings</div>
+
+                 <div className="w-full h-[40px] hover:text-white  hover:bg-blue-700 cursor-pointer flex items-center px-3">Your Ratings</div>
+
+                 <div className="w-full h-[40px] hover:text-white  hover:bg-blue-700 cursor-pointer flex items-center px-3">Saved Professors</div>
+
+                 <div className="w-full h-[40px] hover:text-white  hover:bg-blue-700 cursor-pointer flex items-center px-3"
+                 onClick={()=>{
+                  dispatch(logOut())
+                 }}
+                 >Logout</div>
+                </div>
+                    </>
+                  ) : null
+                }
+                    </>
+                  ) : null
+                }
 
                 <div className="headerMedium:flex hidden cursor-pointer text-[20px]"
                 onClick={()=>{
